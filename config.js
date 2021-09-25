de7d873c45a31fe29e41099a1a9b83766982fff1
Aeeeee-the-Sassy <91150745+Aeeeee-the-Sassy@users.noreply.github.com>
2021-09-25 08:12:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
     app: {
-        px: 'XXX',
-        token: 'XXX',
-        playing: 'by Zerio ❤️',
+        px: '-',
+        token: 'Njc3Nzc1NDc2NTQ2MDc2Njg3.XkZJmg.t_-LjM9x6UPq5JBEe4eSXaQjcN4',
+        playing: 'в котор',
     },
 
     opt: {
         DJ: {
             enabled: false,
-            roleName: 'DJ',
+            roleName: 'DJ Ебан',
             commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
         },
         maxVol: 100,
